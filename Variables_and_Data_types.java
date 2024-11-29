bfb70cc14f8e3a1acdac840f9e9d96ac4d3128ac
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2024-11-29 18:56:21 +0530

    variables and data types in java

---
+++ Variables_and_Data_types.java
@@ -0,0 +1,32 @@
+package Lecture03;
+
+public class Variables_and_Data_types{
+        public static void main(String[] args) {
+            System.out.print("The sum of these numbers are:  ");
+            int num1 = 5;
+            int num2 = 12;
+            int num3 = 21;
+            int sum = num1 + num2 + num3;
+            System.out.println(sum);
+        }
+}
+
+/*
+Variable is a container in which we can insert some value ---like empty water bottle
+
+                           DATA TYPES
+                        /             \
+                       /               \
+                      /                 \
+            Primitive Data Type         Non Primitive Data Type
+            .Int  (4 bytes)                      (Derived from Primitive Data type)
+            .Char (2 bytes)
+            .Float (4 bytes)
+            .byte
+            .long -- bada int no.
+            .short --
+            .double -- bada float no.
+            .bool  -- true/fals
+
+*/
+
