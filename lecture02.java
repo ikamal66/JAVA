da874ce0f401780e8742d87040179f2db7616466
Kamal Singh Dhakad <138019765+ikamal66@users.noreply.github.com>
2024-11-29 18:55:21 +0530

    Naming conversion

---
+++ lecture02.java
@@ -0,0 +1,18 @@
+package Lecture02;
+
+public class lecture02 {
+        public static void main(String[] args) {
+            System.out.println("Second Lecture !");
+
+        /*
+        Naming conversion
+        ---> For classes we use the PascalConvention
+        ---> For Function we use camelCaseConvention
+        EX:-
+        add two numbers
+        AddTwoNumbers ----> PascalConvention  Class
+        addTwoNumbers  ---->  camelCaseConvention Function
+         */
+        }
+    }
+
